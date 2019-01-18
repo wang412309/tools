bdba20e2c91b2eb462397b9c06851c6e83d3cba9
Your Name <you@example.com>
2019-01-18 10:10:31 +0800

    增加一些基础文件

---
+++ src/wang/alipay/Alipay.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace wang\alipay;
+/**
+ * Created by PhpStorm.
+ * User: a
+ * Date: 2019/1/18
+ * Time: 9:59
+ */
+class Alipay
+{
+    
+}
---
+++ src/wang/test/Test.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace wang\test;
+/**
+ * Created by PhpStorm.
+ * User: a
+ * Date: 2019/1/18
+ * Time: 10:01
+ */
+class Test
+{
+    public function test()
+    {
+        return 'test';
+    }
+
+}
